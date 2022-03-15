4b68d230ec634c801cc80b776c21611e316dbd8c
[alper sacmaozu] <[alpersacmaozu@gmail.com]>
2022-03-15 21:00:22 +0300

    csharp_enum
    vsc ile csharp enum

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+          Console.WriteLine(Gunler.Pazar);
+          Console.WriteLine((int)Gunler.Cumartesi);
+
+          int sicaklik=32;
+          if(sicaklik<=(int)HavaDurumu.Normal)
+         
+              Console.WriteLine("Dışarıya çıkmak için ısınmasını bekleyebilirsiniz !");
+              else if(sicaklik>=(int)HavaDurumu.Sicak)
+              Console.WriteLine("Dışarıya çıkmak için sıcak bir gün.");
+              else if(sicaklik>=(int)HavaDurumu.Normal && sicaklik<=(int)HavaDurumu.CokSicak)
+              Console.WriteLine("Hadi dışarıya çıkalım.");
+
+         
+        }
+    }
+    enum Gunler 
+{
+    Pazartesi=1, 
+    Sali, 
+    Carsamba, 
+    Persembe, 
+    Cuma=23, 
+    Cumartesi, 
+    Pazar
+}
+enum HavaDurumu
+{
+Soguk=5,
+Normal=20,
+Sicak=25,
+CokSicak=30
+}
+}
